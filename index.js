134c498b110dc0faae7a7ec1d0e31a8a9affdf4f
Katerina Tuckova <tuckova.mail@gmail.com>
2022-04-22 14:52:47 +0200

    pridani JS souboru s funkcnim prepinanim hracu

---
+++ index.js
@@ -0,0 +1,28 @@
+// kdo je na tahu, zacina hrat krouzek
+let turn = "circle";
+const status = document.querySelector(".status img");
+const btnElm = document.querySelectorAll("button");
+
+const game = (event) => {
+
+// pokud jsou na tahu krouzky, pridej do policka tridu s krouzkem a zmen tah na krizky
+
+  if (turn === "circle") {
+    event.target.classList.add('grid-item--circle');
+    turn = "cross";
+    status.src = "cross.svg";
+    status.alt = "krizek";
+    console.log("hraje krizek")
+  }
+  else {
+    event.target.classList.add('grid-item--cross');
+    turn = "circle";
+    status.src = "circle.svg";
+    status.alt = "krouzek";
+    console.log("hraje krouzek")
+  }
+}
+
+for (let i = 0; i < btnElm.length; i+=1 ) {
+  btnElm[i].addEventListener("click", game);
+}
