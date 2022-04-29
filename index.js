e1190e8c2c8d3add07aa3d9a644fec4d352546f9
Katerina Tuckova <tuckova.mail@gmail.com>
2022-04-29 19:39:49 +0200

    Oznameni vyherce

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // kdo je na tahu, zacina hrat krouzek
 let turn = "circle";
 const status = document.querySelector(".status img");
-const btnElm = document.querySelectorAll(".grid-item button");
+const btnElms = document.querySelectorAll(".grid-item button");
 
 const game = (event) => {
 // policko musi byt prazdne
@@ -13,17 +13,111 @@ const game = (event) => {
     turn = "cross";
     status.src = "cross.svg";
     status.alt = "krizek";
-    console.log("hraje krizek")
   }
   else {
     event.target.classList.add('grid-item--cross');
     turn = "circle";
     status.src = "circle.svg";
     status.alt = "krouzek";
-    console.log("hraje krouzek")
   }
+  console.log(isWinningMove(event.target))
 }
 
-for (let i = 0; i < btnElm.length; i+=1 ) {
-  btnElm[i].addEventListener("click", game);
+for (let i = 0; i < btnElms.length; i+=1 ) {
+  btnElms[i].addEventListener("click", game);
 }
+
+// zjisti jestli je na policku nejaky symbol a jaky, pokud neni tak undefined
+const getSymbol = (button) => {
+  if (button.classList.contains("grid-item--cross")) {
+    return "cross";
+  } else if (button.classList.contains("grid-item--circle")) {
+    return "circle";
+}
+};
+
+// 
+const boardSize = 10;
+const getField = (row, column) => {
+  return btnElms[row * boardSize + column]
+}
+
+console.log(getField(1, 2))
+
+const getPosition = (button) => {
+	let fieldIndex = 0
+	while (fieldIndex < btnElms.length && button !== btnElms[fieldIndex]) {
+		fieldIndex++
+	}
+
+	return {
+		row: Math.floor(fieldIndex / boardSize),
+		column: fieldIndex % boardSize,
+	}
+}
+
+const symbolsToWin = 5
+const isWinningMove = (button) => {
+
+  let winner = "krizky"
+    if (turn === "cross") {
+      winner = "krouzky"
+    }
+  
+  const popup = () => {
+    alert(`Vyhral ${winner}`)
+  }  
+
+	const origin = getPosition(button)
+	const symbol = getSymbol(button)
+
+	let i
+
+	let inRow = 1 // Jednička pro právě vybrané políčko
+	// Koukni doleva
+	i = origin.column
+	while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+		inRow++
+		i--
+	}
+
+	// Koukni doprava
+	i = origin.column
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(origin.row, i + 1))
+	) {
+		inRow++
+		i++
+	}
+
+	if (inRow >= symbolsToWin) {
+		return true
+	}
+
+	let inColumn = 1
+	// Koukni nahoru
+	i = origin.row
+	while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+		inColumn++
+		i--
+	}
+
+	// Koukni dolu
+	i = origin.row
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(i + 1, origin.column))
+	) {
+		inColumn++
+		i++
+	}
+
+	if (inColumn >= symbolsToWin) {
+  
+		return true && setTimeout(popup, 500);
+	}
+
+	return false
+}
+
